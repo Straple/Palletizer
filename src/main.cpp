79d4d1a77e3c28168dec2abdbb5431e3f5a377f1
Straple <evyukhnevich@edu.hse.ru>
2025-11-28 23:28:13 +0300

    update tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,42 @@
+#include <utils/assert.hpp>
+#include <utils/tools.hpp>
+
 #include <iostream>
+#include <fstream>
 
 int main() {
-    std::cout << "HELLO!\n";
+    for (int test = 1;; test++) {
+        std::ifstream input("tests/" + std::to_string(test) + ".csv");
+        if (!input) {
+            break;
+        }
+
+        std::cout << test << std::endl;
+
+        std::ofstream output("tests_cleaned/" + std::to_string(test) + ".csv");
+
+        int x;
+        ASSERT((input >> x) && x == 1, "invalid test");
+        std::string line;
+        std::getline(input, line);
+
+        std::string header;
+        ASSERT(std::getline(input, header), "unable to read header");
+        output << header << std::endl;
+        ASSERT(header == "SKU,Quantity,Length,Width,Height,Weight,Strength,Aisle,Caustic", "invalid header");
+
+
+        while (std::getline(input, line)) {
+            auto data = split(line, ',');
+            ASSERT(data.size() == 9, "invalid data");
+
+            for(int i = 0; i < data.size(); i++){
+                if(i != 0){
+                    output << ',';
+                }
+                output << data[i];
+            }
+            output << std::endl;
+        }
+    }
 }
